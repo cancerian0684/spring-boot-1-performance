c8212957d9da174d3dd52dec641dfe3a8a7238eb
Munish Chandel <cancerian0684@gmail.com>
2018-07-24 18:09:14 +0530

    Initial Project Checkin

---
+++ src/main/java/com/shunya/perf/PerfApp.java
@@ -0,0 +1,12 @@
+package com.shunya.perf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfApp.class, args);
+	}
+}
